3cc0ac995507025c5b3e8ee234ea0e3b30a30721
hamzah khan <hamzah.khan1996@yahoo.com>
2021-10-08 00:59:26 +0100

    added new tuttorial and tested new pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>this is the index page</h1>
+    <Tutorial />
+  </div>
 </template>
 
 <script>
---
+++ pages/temp.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+  <h1>this is the temp page</h1>
+
+</div>
+</template>
+
+<script>
+export default {}
+</script>
