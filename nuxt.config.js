7ed64998539b050413ebbb6f300d3b72892c1990
hamzah khan <hamzah.khan1996@yahoo.com>
2021-10-12 22:17:46 +0100

    fixed some errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: false,
+  //ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -57,8 +57,9 @@ export default {
   build: {
   },
   generate: {
-    dir: 'docs',
-    // fallback: true
+    dir:'docs'
+    //fallback: true
+
   }
 
 }
