88cd4ad1e0fdc3a0d880df3370182291c6913845
hamzah khan <hamzah.khan1996@yahoo.com>
2021-10-12 21:55:55 +0100

    regenerated content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: false,
+  // ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -57,7 +57,8 @@ export default {
   build: {
   },
   generate: {
-    dir:'docs'
+    dir: 'docs',
+    // fallback: true
   }
 
 }
