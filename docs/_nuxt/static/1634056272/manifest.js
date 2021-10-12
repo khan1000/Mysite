d5d3fa9a37c9b9ca140a517d44d365062e222d08
hamzah khan <hamzah.khan1996@yahoo.com>
2021-10-12 20:33:20 +0100

    disable server side rendering

--- docs/_nuxt/static/1634056272/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontenttest","\u002Ftemp","\u002Fblog"]})
--- docs/_nuxt/static/1634056272/temp/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634056272",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"716be392"}}}}(null));
