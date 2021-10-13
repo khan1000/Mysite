2fa35b842ac0bfe80c14b9c374f9a1b95d894285
hamzah khan <hamzah.khan1996@yahoo.com>
2021-10-13 02:35:54 +0100

    added bootstrap to head

--- docs/_nuxt/static/1634073401/contenttest/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634073401",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontenttest",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0e7c5e41"}}}}(null));
