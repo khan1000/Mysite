8e0d32ec6d81dbf78dcbc86b5e8d06d742aaa43b
hamzah khan <hamzah.khan1996@yahoo.com>
2021-10-14 01:45:45 +0100

    reverted changes

---
+++ docs/_nuxt/static/1634172296/temp/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634172296",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftemp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b625db90"}}}}(null));
